998be9986082316f8910fa34704b3add1c5cb5ee
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2019-05-30 20:46:20 +0300

    Rename DurationMeasured to TimedValue, withMeasureTime to measuredTimeValue

--- libraries/stdlib/src/kotlin/time/measureTime.kt
+++ libraries/stdlib/src/kotlin/time/measureTime.kt
@@ -42,35 +42,34 @@ public inline fun Clock.measureTime(action: () -> Unit): Duration {
  * @property value the result of the action.
  * @property duration the time elapsed to execute the action.
  */
-public data class DurationMeasured<T>(val value: T, val duration: Duration)
+public data class TimedValue<T>(val value: T, val duration: Duration)
 
-// or runMeasured
 /**
- * Executes the given [action] and returns an instance of [DurationMeasured] class, containing both
+ * Executes the given [action] and returns an instance of [TimedValue] class, containing both
  * the result of the [action] execution and the duration of elapsed time interval.
  *
  * The elapsed duration is measured according to [MonoClock].
  */
-public inline fun <T> withMeasureTime(action: () -> T): DurationMeasured<T> {
+public inline fun <T> measureTimedValue(action: () -> T): TimedValue<T> {
     contract {
         callsInPlace(action, InvocationKind.EXACTLY_ONCE)
     }
 
-    return MonoClock.withMeasureTime(action)
+    return MonoClock.measureTimedValue(action)
 }
 
 /**
- * Executes the given [action] and returns an instance of [DurationMeasured] class, containing both
+ * Executes the given [action] and returns an instance of [TimedValue] class, containing both
  * the result of the [action] execution and the duration of elapsed time interval.
  *
  * The elapsed duration is measured according to the specified `this` [Clock] instance.
  */
-public inline fun <T> Clock.withMeasureTime(action: () -> T): DurationMeasured<T> {
+public inline fun <T> Clock.measureTimedValue(action: () -> T): TimedValue<T> {
     contract {
         callsInPlace(action, InvocationKind.EXACTLY_ONCE)
     }
 
     val mark = mark()
     val result = action()
-    return DurationMeasured(result, mark.elapsed())
+    return TimedValue(result, mark.elapsed())
 }
